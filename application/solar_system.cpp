3f0f4cd7cea89395fa77d9bee3b31102121571e7
Fernando <fernando.cardenasm@gmail.com>
2015-12-15 20:21:19 +0100

    Add screensquad plus framebuffer

--- application/solar_system.cpp
+++ application/solar_system.cpp
@@ -79,6 +79,7 @@ struct model_object {
 };
 model_object planet_object;
 model_object star_object;
+model_object squadscreen_object;
 //model_object orbit_object;
 
 // camera matrices
@@ -134,12 +135,23 @@ GLfloat quadVertices[] = {   // Vertex attributes for a quad that fills the enti
 	1.0f,  1.0f,  1.0f, 1.0f
 };
 
+std::vector<float> screenQuadVertices{   // Vertex attributes for a quad.						 // Positions   // Uv
+	-1.0f,  -1.0f,  0.0f, 0.0f, 0.0f,
+	1.0f, -1.0f,  0.0f, 1.0f, 0.0f,
+	-1.0f,  1.0f,  0.0f, 0.0f, 1.0f,
+	1.0f, 1.0f,  0.0f, 1.0f, 1.0f,
+	
+};
+
 // Framebuffers
 GLuint framebuffer;
 // Color attachment texture
 GLuint textureColorbuffer;
 // Create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
 GLuint rbo;
+
+// Setup screen VAO
+GLuint quadVAO, quadVBO;
 
 /////////////////////////// forward declarations //////////////////////////////
 void quit(int status);
@@ -153,6 +165,10 @@ void show_fps();
 void render(float scale_factor, float translation_factor, float rotation_factor, int number_planet);
 void rendermoon(float scale_factor, float translation_factor, float rotation_factor);
 void renderstar(float scale_factor, float translation_factor, float rotation_factor);
+void initializeFrameBuffer(int width, int height);
+void updateFrameBuffer(int width, int height);
+void renderscreensquad();
+GLuint generateAttachmentTexture(bool depth, bool stencil, int width, int height);
 //void renderorbit(float translation_factor);
 /////////////////////////////// main function /////////////////////////////////
 int main(int argc, char* argv[]) {
@@ -237,6 +253,9 @@ int main(int argc, char* argv[]) {
   update_projection(window, width, height);
   update_view();
 
+
+  initializeFrameBuffer(width, height);
+
   // set up models
   initialize_geometry();
 
@@ -249,8 +268,11 @@ int main(int argc, char* argv[]) {
     // query input
     glfwPollEvents();
     // clear buffer
+	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     // draw geometry
+	
+  
   shader_active = 0;
   update_shader_programs(0);
 
@@ -280,6 +302,13 @@ int main(int argc, char* argv[]) {
   shader_active = 1;
   update_shader_programs(1);
   renderstar(0.03, 0.0f, 0.0);
+
+  //Render screensquad
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  shader_active = 2;
+  update_shader_programs(2);
+  renderscreensquad();
 
     // swap draw buffer to front
     glfwSwapBuffers(window);
@@ -357,6 +386,59 @@ void initialize_geometry() {
 	// configure currently bound array buffer
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(float) * v_star_position.size(), &v_star_position[0], GL_STATIC_DRAW);
 	
+	//Squadscreen
+	
+	//First solution
+	// generate vertex array object
+	glGenVertexArrays(1, &squadscreen_object.vertex_AO);
+	// bind the array for attaching buffers
+	glBindVertexArray(squadscreen_object.vertex_AO);
+
+	// generate generic buffer
+	glGenBuffers(1, &squadscreen_object.vertex_BO);
+	// bind this as an vertex array buffer containing all attributes
+	glBindBuffer(GL_ARRAY_BUFFER, squadscreen_object.vertex_BO);
+	// configure currently bound array buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * screenQuadVertices.size(), &screenQuadVertices[0], GL_STATIC_DRAW);
+
+	// activate first attribute on gpu
+	glEnableVertexAttribArray(0);
+	// first attribute is 3 floats with no offset & stride
+	//1st: The specific attribute, 2nd: , 3rd: the type, 5th: 3 positions + 3 type of colors, 6th: reference of start of each starts attribute
+	glVertexAttribPointer(0, model::POSITION.components, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (GLvoid*)(0));
+	// activate second attribute on gpu
+	glEnableVertexAttribArray(1);
+	// second attribute is 3 floats with no offset & stride
+	glVertexAttribPointer(1, model::NORMAL.components, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (GLvoid*)(3 * sizeof(float)));
+
+	// generate generic buffer
+	glGenBuffers(1, &squadscreen_object.element_BO);
+	// bind this as an vertex array buffer containing all attributes
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, squadscreen_object.element_BO);
+	// configure currently bound array buffer
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(float) * screenQuadVertices.size(), &screenQuadVertices[0], GL_STATIC_DRAW);
+
+
+	/*
+	glGenVertexArrays(1, &quadVAO);
+	glGenBuffers(1, &quadVBO);
+	glBindVertexArray(quadVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (GLvoid*)(2 * sizeof(GLfloat)));
+	glBindVertexArray(0);
+	*/
+	// generate generic buffer
+	//glGenBuffers(1, &star_object.element_BO);
+	// bind this as an vertex array buffer containing all attributes
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, star_object.element_BO);
+	// configure currently bound array buffer
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(float) * v_star_position.size(), &v_star_position[0], GL_STATIC_DRAW);
+
+
 	// Texture 0 Sun
 	// ====================
 	glActiveTexture(GL_TEXTURE0);
@@ -689,6 +771,58 @@ void renderstar(float scale_factor, float translation_factor, float rotation_fac
 
 }
 
+//Method to render the screesquad
+void renderscreensquad()
+{
+
+	float now = glfwGetTime();
+	float noww = now*0.05;
+	//model_matrix = glm::translate(model_matrix, glm::vec3{ translation_factor, 0.0f, 0.0f });
+
+	//rotate it aroound y-axis
+	//model_matrix = glm::rotate(model_matrix, float(now*0.01), glm::vec3{ 0.0f,1.0f, 0.0f });
+
+	///////
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+	// extra matrix for normal transformation to keep them orthogonal to surface
+	//How to render it?
+
+	glm::mat4 model_matrix = glm::mat4{};
+	//model_matrix = glm::scale(model_matrix, glm::vec3(0.1));
+
+	//model_matrix = glm::translate(model_matrix, glm::vec3{ translation_factor, 0.0f, 0.0f });
+
+	//rotate it aroound y-axis
+	//model_matrix = glm::rotate(model_matrix, float(now*0.01), glm::vec3{ 0.0f,1.0f, 0.0f });
+
+	///////
+	glUniformMatrix4fv(location_model_matrix, 1, GL_FALSE, glm::value_ptr(model_matrix));
+
+	// extra matrix for normal transformation to keep them orthogonal to surface
+
+	//Screen Texture Frag
+	int color_sampler_location = glGetUniformLocation(simple_program, "screenTexture");
+	glUseProgram(simple_program);
+	glUniform1i(color_sampler_location, 0);
+
+	glm::mat4 normal_matrix = glm::inverseTranspose(glm::inverse(camera_transform) * model_matrix);
+
+	glUniformMatrix4fv(location_normal_matrix, 1, GL_FALSE, glm::value_ptr(normal_matrix));
+
+	////planet color 
+	//glUniform3fv(location_color_matrix, 1, textureColorbuffer);
+	/////
+
+	glBindVertexArray(squadscreen_object.vertex_AO);
+
+	utils::validate_program(simple_program);
+
+	// draw bound vertex array as triangles using bound shader
+
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, sizeof(squadscreen_object) * 5);
+
+}
 
 ///////////////////////////// update functions ////////////////////////////////
 // update viewport and field of view
@@ -706,6 +840,8 @@ void update_projection(GLFWwindow* window, int width, int height) {
   camera_projection = glm::perspective(fov_y, aspect, 0.1f, 10.0f);
   // upload matrix to gpu
   glUniformMatrix4fv(location_projection_matrix, 1, GL_FALSE, glm::value_ptr(camera_projection));
+
+  //updateFrameBuffer(width, height);
 }
 
 // update camera transformation
@@ -728,11 +864,16 @@ void update_shader_programs(int shader) {
       new_program = shader_loader::program(resource_path + "shaders/simple.vert",
         resource_path + "shaders/simple.frag");
     }
-    else {
+    else if(shader == 1) {
       //shader for the star
       new_program = shader_loader::program(resource_path + "shaders/starshader.vert",
         resource_path + "shaders/starshader.frag");
     }
+	else if (shader == 2) {
+		//shader for the screensquad
+		new_program = shader_loader::program(resource_path + "shaders/screensquad.vert",
+			resource_path + "shaders/screensquad.frag");
+	}
 
     // free old shader
     glDeleteProgram(simple_program);
@@ -811,4 +952,92 @@ void quit(int status) {
   glfwTerminate();
 
   std::exit(status);
+}
+
+// Generates a texture that is suited for attachments to a framebuffer
+GLuint generateAttachmentTexture(bool depth, bool stencil, int screenWidth, int screenHeight)
+{
+	// What enum to use?
+	GLenum attachment_type;
+	if (!depth && !stencil)
+		attachment_type = GL_RGB;
+	else if (depth && !stencil)
+		attachment_type = GL_DEPTH_COMPONENT;
+	else if (!depth && stencil)
+		attachment_type = GL_STENCIL_INDEX;
+
+	//Generate texture ID and load texture data
+	GLuint textureID;
+	//glActiveTexture(GL_TEXTURE0);
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	if (!depth && !stencil)
+		glTexImage2D(GL_TEXTURE_2D, 0, (GLint)attachment_type,screenWidth , screenHeight, 0, attachment_type, GL_UNSIGNED_BYTE, NULL);
+	else // Using both a stencil and depth test, needs special format arguments
+		glTexImage2D(GL_TEXTURE_2D, 0, (GLint)GL_DEPTH24_STENCIL8, screenWidth, screenHeight, 0, GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, (GLint)GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, (GLint)GL_LINEAR);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	return textureID;
+}
+
+void initializeFrameBuffer(int width, int height) {
+	//Framebuffer
+
+	glGenFramebuffers(1, &framebuffer);
+	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+	// Create a color attachment texture
+	textureColorbuffer = generateAttachmentTexture(false, false, width, height);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
+	// Create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
+
+	glGenRenderbuffers(1, &rbo);
+	glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height); // Use a single renderbuffer object for both a depth AND stencil buffer.
+	//glBindRenderbuffer(GL_RENDERBUFFER, 0);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo); // Now actually attach it
+
+																								  //GLenum draw_buffers[1] = { GL_COLOR_ATTACHMENT0};
+																								  //glDrawBuffers(1, draw_buffers);
+																								  // Now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+		std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!";
+	}
+	else {
+		std::cout << "Framebuffer is complete!";
+	}
+
+	//glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void updateFrameBuffer(int width, int height) {
+	//Framebuffer
+
+	//glGenFramebuffers(1, &framebuffer);
+	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+	// Create a color attachment texture
+	textureColorbuffer = generateAttachmentTexture(false, false, width, height);
+	//glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
+	// Create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
+
+	//glGenRenderbuffers(1, &rbo);
+	glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+	//glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height); // Use a single renderbuffer object for both a depth AND stencil buffer.
+																				//glBindRenderbuffer(GL_RENDERBUFFER, 0);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo); // Now actually attach it
+
+																								  //GLenum draw_buffers[1] = { GL_COLOR_ATTACHMENT0};
+																								  //glDrawBuffers(1, draw_buffers);
+																								  // Now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+		std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!";
+	}
+	else {
+		std::cout << "Framebuffer is complete!";
+	}
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
